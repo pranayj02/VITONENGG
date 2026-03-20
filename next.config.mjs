486cb1805546e882d813fba59ac569e1e014c940
pranayj02 <pranay0609@gmail.com>
2026-03-20 15:51:19 +0530

    Configure unoptimized images in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-export default nextConfig;
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
