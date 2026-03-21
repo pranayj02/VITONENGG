a0cc1c590fcd613e9f4cabe7037ff371b58f0dd1
Pranay Jain <pranay0609@.com>
2026-03-21 23:26:10 +0530

    FY Change

--- lib/fy.ts
+++ lib/fy.ts
@@ -3,5 +3,5 @@ export function getCurrentFY(today = new Date()) {
   const fyStartYear = today.getMonth() >= 3 ? year : year - 1;
   const yy = String(fyStartYear).slice(-2);
   const nextYy = String(fyStartYear + 1).slice(-2);
-  return `${yy}/${nextYy}`;
+  return `${yy}-${nextYy}`;
 }
